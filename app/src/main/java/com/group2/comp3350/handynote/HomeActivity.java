ff79dca14b3a35811e6eb468327570584193ea33
Matthias <saiihttam@gmail.com>
2016-02-18 02:22:34 -0600

    Added Mainpage for adding photo and photo activity

--- app/src/main/java/com/group2/comp3350/handynote/HomeActivity.java
+++ app/src/main/java/com/group2/comp3350/handynote/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.group2.comp3350.handynote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,19 +19,6 @@ public class HomeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -55,5 +44,11 @@ public class HomeActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_AddPhotoActivityBtn(View view)
+    {
+        Intent intent = new Intent(HomeActivity.this, AddPhotoActivity.class);
+        startActivity(intent);
     }
 }
