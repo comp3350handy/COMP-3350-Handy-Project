e6260b2b9fe5b0a317a43305b343698875841409
Matthias <saiihttam@gmail.com>
2016-02-18 01:42:25 -0600

    Made it Gradle-based project. i.e. now you can add java classes and run them

---
+++ app/src/androidTest/java/com/group2/comp3350/handynote/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.group2.comp3350.handynote;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
